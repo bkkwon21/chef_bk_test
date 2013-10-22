5768dc53e2dbb5b0f84e2e324ee41cb98c3f5cdd
bkwon <bong.kwon@nokia.com>
2013-10-22 16:37:51 -0400

    update

--- tomcat/recipes/install.rb
+++ tomcat/recipes/install.rb
@@ -11,26 +11,7 @@
 # or implied. See the License for the specific language governing permissions
 # and limitations under the License.
 
-tomcat_pkgs = value_for_platform(
-  ['debian', 'ubuntu'] => {
-    'default' => ["tomcat#{node['tomcat']['base_version']}", 'libtcnative-1', 'libmysql-java']
-  },
-  ['centos', 'redhat', 'fedora', 'amazon'] => {
-    'default' => ["tomcat#{node['tomcat']['base_version']}", 'tomcat-native', 'mysql-connector-java']
-  },
-  'default' => ["tomcat#{node['tomcat']['base_version']}"]
-)
-
-tomcat_pkgs.each do |pkg|
-  package pkg do
+rpm_package "jdk-1.6.0_45" do
+    source "https://github.com/bkkwon21/chef_bk_test/blob/master/rpms/jdk-6u45-linux-amd64.rpm"
     action :install
-  end
 end
-
-link ::File.join(node['tomcat']['lib_dir'], node['tomcat']['mysql_connector_jar']) do
-  to ::File.join(node['tomcat']['java_dir'], node['tomcat']['mysql_connector_jar'])
-  action :create
-end
-
-# remove the ROOT webapp, if it got installed by default
-include_recipe 'tomcat::remove_root_webapp'
